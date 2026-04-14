f89494807574115ecc4130ff20fda6750bc1055b
HieuPD123 <hieu151005@gmail.com>
2026-04-14 22:47:17 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import HeroSlider from "@/components/homepage/home/hero-slider";
-import ScheduleList from "@/components/homepage/home/schedule-list";
-import TrendingNow from "@/components/homepage/home/trending-now";
+
 
 export default function Home() {
   return (
     <div>
-      <HeroSlider/>
-      <TrendingNow/>
-      <ScheduleList/>
+      <p>Hello World</p>
     </div>
   );
 }
